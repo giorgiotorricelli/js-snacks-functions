867def85f1ccd86a52963c715773d129a34f6269
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-01 16:27:19 +0200

    esercizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
@@ -5,9 +7,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function filterInitial(namesArray, filterChar){
+    const filteredArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        if (namesArray[i][0].toLowerCase() === filterChar){
+            filteredArray.push(namesArray[i]);
+        }
+    }
+    return filteredArray
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const risultato = filterInitial(names, "a")
+console.log(risultato);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
