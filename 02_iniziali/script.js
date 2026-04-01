82e142c380ccd6c256e472b11719ef6036e04799
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-01 16:15:31 +0200

    esercizio 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
@@ -5,8 +7,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function justInitial(arrayWithNames) {
+    const initials = [];
+    for (let i = 0; i < arrayWithNames.length; i++) {
+        initials.push(arrayWithNames[i][0]);
+    }
+    return initials;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const risultato = justInitial(names);
+console.log(risultato);
+
 
 
 
