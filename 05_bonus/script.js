e45f474893be957c49330fc926c51787a6318439
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-01 17:42:59 +0200

    esercizio bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
@@ -5,14 +7,35 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const myName = 'Mario';
 
 
 // Dichiara la funzione qui.
 
 
+function greetings(name){
+    const time = Math.floor(Math.random() * 25).toFixed(2);
+    let saluto = "";
+    let result = "";
+
+    if (time > 4 && time < 13) {
+        saluto = "Buongiorno";
+    } else if (time > 12 && time < 17) {
+        saluto = "Buon pomeriggio";
+    } else if (time > 16 && time < 22) {
+        saluto = "Buona sera";
+    } else {
+        saluto = "Buona notte";
+    }
+
+    result = `${saluto} ${name}. (orario -> ${time})`;
+    return result;
+}
+
 // Invoca la funzione qui e stampa il risultato in console
 
+const risultato = greetings(myName);
+console.log(risultato);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
