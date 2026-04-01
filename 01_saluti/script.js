bbb30ff80c73501b59ebd8ed57122a0608fda6be
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-01 16:05:43 +0200

    esercizio 1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
@@ -5,8 +7,17 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+function greeting(name) {
+    return `Ciao ${name}`;
+}
+
+//const greeting2 = nome => `Ciao ${nome}`;
+
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const risultato = greeting(userName);
+console.log(risultato);
 
 
 
